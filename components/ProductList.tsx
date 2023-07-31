92b974243e971f999348b75cb39772576545e4f8
zeroluffs <zeroluffs@gmail.com>
2023-07-31 14:40:14 -0500

    product list

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,22 +1,129 @@
 import { ProductCard } from '@/components/ProductCard'
 import { useProducts } from '@/hooks/useProducts'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import { ChangeEvent, useCallback, useState } from 'react'
+import { useQuery, useQueryClient } from 'react-query'
+import { fetchProducts } from '@/Services/Event'
 
 export function ProductList() {
-  const { data, fetchNextPage, hasNextPage } = useProducts()
+  const [isPriceActive, setIsPriceActive] = useState(false)
+  const [isRatingActive, setIsRatingActive] = useState(false)
+  const queryClient = useQueryClient()
+
+  // Function to handle filtering by price
+  const filterProducts = useCallback(
+    (data: FetchResponse) => {
+      if (isPriceActive || isRatingActive) {
+        if (isPriceActive) {
+          data?.products?.sort((a, b) => a.price - b.price)
+        }
+        if (isRatingActive) {
+          data?.products?.sort((a, b) => a.rating - b.rating)
+        }
+        return data
+      } else {
+        return data
+      }
+    },
+    [isPriceActive, isRatingActive]
+  )
+  const [page, setPage] = useState(0)
+  const [inputText, setInputText] = useState('')
+
+  const { isLoading, error, data, isFetching, isPreviousData } = useQuery<FetchResponse>({
+    queryKey: ['products', page],
+
+    queryFn: () => fetchProducts(page),
+    keepPreviousData: true,
+    select: filterProducts,
+  })
+  let inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let lowerCase = (e.target as HTMLInputElement).value.toLowerCase()
+    setInputText(lowerCase)
+  }
+  let numberOfPages = data!?.total / data!?.limit || 0
+  // let products = data?.products || []
+  let products = data!?.products?.filter((product) => {
+    if (inputText === '') {
+      return product
+    } else {
+      return product.title.toLowerCase().includes(inputText)
+    }
+  })
+  const handleFilterByPrice = () => {
+    setIsPriceActive(!isPriceActive)
+    if (isRatingActive) {
+      setIsRatingActive(false)
+    }
+  }
+
+  // Function to handle filtering by rating
+  const handleFilterByRating = () => {
+    setIsRatingActive(!isRatingActive)
+    if (isPriceActive) {
+      setIsPriceActive(false)
+    }
+  }
+  if (isLoading) return 'Loading...'
+  if (error) {
+    // @ts-ignore
+    return 'An error has occurred: ' + error.message
+  }
   return (
-    <InfiniteScroll
-      next={fetchNextPage}
-      hasMore={hasNextPage || false}
-      loader={<p>Loading...</p>}
-      dataLength={data?.pages?.reduce((total, page) => total + page.length, 0) || 0}>
-      <div className={'flex justify-center items-center'}>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 ">
-          {data?.pages?.map((page, index) =>
-            page?.map((product, index) => <ProductCard key={index} {...product} />)
-          )}
+    <div className={'mt-[64px]'}>
+      <div className={'flex flex-col gap-6'}>
+        <input
+          onChange={inputHandler}
+          placeholder={'Search Product'}
+          className={'bg-gray-300 rounded-md p-3  w-[244px] h-[32px]'}
+          type="text"
+        />
+        <div className="flex space-x-4">
+          <button
+            onClick={handleFilterByPrice}
+            className={`border p-2 ${
+              isPriceActive ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'
+            }`}>
+            Filter by Price
+          </button>
+          <button
+            onClick={handleFilterByRating}
+            className={`border p-2 ${
+              isRatingActive ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'
+            }`}>
+            Filter by Rating
+          </button>
         </div>
       </div>
-    </InfiniteScroll>
+
+      <div className={'flex justify-center items-center mt-8'}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 ">
+          {products?.map((product, index) => (
+            <ProductCard key={index} {...product} />
+          ))}
+        </div>
+      </div>
+      {/*<span>Current Page: {page + 1}</span>*/}
+      <div className={'flex  flex-row justify-center gap-6 my-6'}>
+        <button
+          className={'text-xl text-blue-500'}
+          onClick={() => setPage((old) => Math.max(old - 1, 0))}
+          disabled={page === 0}>
+          Previous Page
+        </button>{' '}
+        <button
+          className={'text-xl text-blue-500'}
+          onClick={() => {
+            if (!isPreviousData) {
+              setPage((old) => old + 1)
+            }
+          }}
+          // Disable the Next Page button until we know a next page is available
+          disabled={page === numberOfPages}>
+          Next Page
+        </button>
+        {isFetching ? <span> Loading...</span> : null} {/*<ProductList />*/}
+      </div>
+    </div>
   )
 }
