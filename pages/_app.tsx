1325a590ed4b8aa54deca2dbe86ef5248a7d1030
zeroluffs <zeroluffs@gmail.com>
2023-07-31 18:22:37 -0500

    fixed import error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { Hydrate, QueryClient, QueryClientProvider, useQuery } from 'react-query
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from '@/store/store'
-import { Head } from 'next/document'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
