79a11bbc719a91531525b1b2f5c2ce8e930bc63a
zeroluffs <zeroluffs@gmail.com>
2023-07-31 10:59:08 -0500

    truncating product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 export function ProductCard(product: Product) {
+  const [showFullText, setShowFullText] = useState(false)
+  const longText = product.description
   return (
     <div className="w-[300px] flex flex-col gap-[12px] shadow-md rounded-sm">
       <div>
@@ -8,7 +11,21 @@ export function ProductCard(product: Product) {
       </div>
       <div className="mx-2">
         <h1 className="text-lg font-semibold">{product.title}</h1>
-        <p>{product.description}</p>
+        {/*<p className={'truncate'}>{product.description}</p>*/}
+        <div>
+          {showFullText ? (
+            <p>{longText}</p>
+          ) : (
+            <p>{longText.length > 100 ? longText.slice(0, 100) + '...' : longText}</p>
+          )}
+          {longText.length > 100 && (
+            <button
+              className={'text-sm text-blue-500'}
+              onClick={() => setShowFullText(!showFullText)}>
+              {showFullText ? 'Show Less' : 'Show More'}
+            </button>
+          )}
+        </div>
         <p>${product.price}</p>
         <p>Reviews: {product.rating}</p>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,30 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useQuery } from "react-query";
-import { fetchProducts } from "@/Services/Event";
-import { ProductCard } from "@/components/ProductCard";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { useQuery } from 'react-query'
+import { fetchProducts } from '@/Services/Event'
+import { ProductCard } from '@/components/ProductCard'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { isLoading, error, data } = useQuery<Product[]>(
-    "products",
-    fetchProducts
-  );
-  let products = data || [];
-  if (isLoading) return "Loading...";
+  const { isLoading, error, data } = useQuery<Product[]>('products', fetchProducts)
+  let products = data || []
+  if (isLoading) return 'Loading...'
   if (error) {
     // @ts-ignore
-    return "An error has occurred: " + error.message;
+    return 'An error has occurred: ' + error.message
   }
 
   return (
-    <div>
-      <h1>{products[0].title}</h1>
-      <ProductCard {...products[0]} />
+    <div className="max-w-7xl m-auto">
+      <div className={'flex justify-center items-center'}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 ">
+          {products.map((product, index) => (
+            <ProductCard key={index} {...product} />
+          ))}
+        </div>
+      </div>
+      d{' '}
     </div>
-  );
+  )
 }
