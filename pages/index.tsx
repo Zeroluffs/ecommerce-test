f8269ad943e670b0f610a3c3a0472e9fd2fb20d1
zeroluffs <zeroluffs@gmail.com>
2023-07-31 09:45:12 -0500

    fetching product service

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useQuery } from "react-query";
+import { fetchProducts } from "@/Services/Event";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("https://dummyjson.com/products").then((res) => res.json())
+  const { isLoading, error, data } = useQuery<Product[]>(
+    "products",
+    fetchProducts
   );
+  let products = data || [];
   if (isLoading) return "Loading...";
   if (error) {
     // @ts-ignore
     return "An error has occurred: " + error.message;
   }
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>{products[0].title}</h1>
+    </div>
+  );
 }
