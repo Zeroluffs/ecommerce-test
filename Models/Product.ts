4336d2f3ecfd0b2eefb639bb4deaadf6c576669e
zeroluffs <zeroluffs@gmail.com>
2023-07-31 09:27:46 -0500

    product class

---
+++ Models/Product.ts
@@ -0,0 +1,22 @@
+class Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+
+  // constructor(id: number, name: string, price: number){
+  //     this.id = id;
+  //     this.name = name;
+  //     this.price = price;
+  // }
+  // showProduct(){
+  //     console.log(`ID: ${this.id}, Name: ${this.name}, Price: ${this.price}`);
+  // }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useQuery } from "react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main></main>;
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("https://dummyjson.com/products").then((res) => res.json())
+  );
+  if (isLoading) return "Loading...";
+  if (error) {
+    // @ts-ignore
+    return "An error has occurred: " + error.message;
+  }
+
+  return <div></div>;
 }
