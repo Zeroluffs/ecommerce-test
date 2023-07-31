4b91525cf3ceb68efd11078bd0df200c59ea7b62
zeroluffs <zeroluffs@gmail.com>
2023-07-31 15:54:44 -0500

    cart slice

---
+++ slices/cart.ts
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk, current } from '@reduxjs/toolkit'
+
+const initialState = {
+  items: [] as Product[],
+  status: 'idle',
+  error: null,
+}
+export const addToCart = createAsyncThunk('cart/addToCart', async (product: Product) => {
+  return product
+})
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialState,
+  extraReducers(builder) {
+    builder.addCase(addToCart.fulfilled, (state, action) => {
+      state.items.push(action.payload)
+    })
+  },
+  reducers: {},
+})
+
+export default cartSlice.reducer
+
+export const numberOfItems = (state: { cart: { items: string | any[] } }) => state.cart.items.length
+export const totalPrice = (state: { cart: { items: any[] } }) => {
+  let total = 0
+  state.cart.items.map((item) => {
+    total += item.price
+  })
+  return total
+}
